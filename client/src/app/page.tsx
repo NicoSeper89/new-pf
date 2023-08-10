5c50ca08f8dff38867e4adc19a34b515ab463ca7
nicoseper89 <nicosepertino@hotmail.com>
2023-08-10 02:55:06 -0300

    background, font and basic navBar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-start">
-      
-    </main>
-  )
+    <>
+      <div className="flex items-center self-end justify-center gap-16 py-4 px-10 font-light text-[1.2rem]">
+        <div className=" py-2 px-6">HOME</div>
+        <div className=" py-2 px-6">SKILLS</div>
+        <div className=" py-2 px-6">PROJECT</div>
+        <div className=" py-2 px-6">ABOUT ME</div>
+        <div className=" py-2 px-6">CONTACT</div>
+      </div>
+    </>
+  );
 }
