2726f21518d7d1013d756d14df266eb6e9e0c53e
nicoseper89 <nicosepertino@hotmail.com>
2023-09-25 00:41:31 -0300

    add plane scene

--- client/src/app/components/HomeScene.tsx
+++ client/src/app/components/HomeScene.tsx
@@ -12,33 +12,34 @@ const HomeScene: React.FC = () => {
     if (!canvasRef.current) return;
 
     const scene = new THREE.Scene();
-
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    camera.position.set(10, 5, -10);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(0, 1, 3);
+    camera.lookAt(new THREE.Vector3(0, 1.05, 0));
 
-    const light = new THREE.AmbientLight(0x505050); 
-    light.intensity = 100;
+    const light = new THREE.DirectionalLight(0x505050);
+    light.position.set(0, 10, 10);
+    light.intensity = 50;
     scene.add(light);
 
     const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const controls = new OrbitControls(camera, canvasRef.current);
+    /* const controls = new OrbitControls(camera, canvasRef.current);
     controls.target.set(0, 0, 0);
-    controls.update();
+    controls.update(); */
 
     const loader = new GLTFLoader();
 
     loader.load(
       "planoEscena.glb",
       function (gltf) {
-        scene.add(gltf.scene);
+        const plane = gltf.scene;
+        scene.add(plane);
       },
       undefined,
       function (error) {
