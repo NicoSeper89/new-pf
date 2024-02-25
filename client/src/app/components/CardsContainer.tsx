df3013c6c47507cb383def5ed4b185a5a750cc2e
nicoseper89 <nicosepertino@hotmail.com>
2024-02-24 21:19:51 -0300

    change project card styles.

--- client/src/app/components/CardsContainer.tsx
+++ client/src/app/components/CardsContainer.tsx
@@ -15,68 +15,91 @@ export interface CardInfo {
   image: string;
   repository_link: string;
   deploy_link: string;
+  svg_path: string;
+  hex_color: string;
 }
 
 export interface Props {
   cards: CardInfo[];
 }
 
-const CardsContainer: React.FC<Props> = ({ cards }) => {
-  const [selectedCard, setSelectedCard] = useState<Number>(0);
+const variantsBox = {
+  selected: () => ({
+    rotateY: [0, 180, 0],
+    transition: {
+      duration: 2.2,
+      type: "tween"
+    }
+  }),
+  unselected: (color: string) => ({
+    rotateY: 0,
+  }),
+};
 
-  const handleClick = (index: Number) => {
-    setSelectedCard(index);
+const CardsContainer: React.FC<Props> = ({ cards }) => {
+  const [selectedCardIndex, setselectedCardIndex] = useState<number>(0);
+
+  const handleClick = (index: number) => {
+    setselectedCardIndex(index);
   };
 
   return (
     <>
       <div className="flex flex-col items-start justify-start w-full h-full">
-        <h2 className="text-4xl">{cards[selectedCard.valueOf()].position}</h2>
+        <h2 className="text-4xl">{cards[selectedCardIndex].position}</h2>
         <h3 className="text-3xl">
-          {cards[selectedCard.valueOf()].organization}
+          {cards[selectedCardIndex].organization}
         </h3>
-        <div className="relative w-full h-1 bg-red-600 my-2" />
-        <p className="text-justify">{cards[selectedCard.valueOf()].description}</p>
+        <div className="relative w-full h-1 bg-[#ff200380] my-2" />
+        <p className="text-justify">
+          {cards[selectedCardIndex].description}
+        </p>
         <br />
-        <span>{cards[selectedCard.valueOf()].date.slice(0, 7)}</span>
+        <span>{cards[selectedCardIndex].date.slice(0, 7)}</span>
         <br />
-        <Link href={cards[selectedCard.valueOf()].repository_link} target="_blank">Repository</Link>
+        <Link
+          href={cards[selectedCardIndex].repository_link}
+          target="_blank"
+        >
+          Repository
+        </Link>
       </div>
       <AnimatePresence>
         <div className="flex w-full justify-between items-center">
           <button
             onClick={() => {
               let cardNumber =
-                selectedCard == 0 ? cards.length - 1 : selectedCard.valueOf() - 1;
-              setSelectedCard(cardNumber);
+                selectedCardIndex == 0
+                  ? cards.length - 1
+                  : selectedCardIndex.valueOf() - 1;
+              setselectedCardIndex(cardNumber);
             }}
           >
-            Prev
+            {"<"}
           </button>
-          <div className="flex flex-wrap">
+          <div className="flex h-[12rem]">
             {cards.map((card, index) => (
               <motion.div
-                className="relative h-[8rem] w-[9rem] overflow-hidden"
+                className={`flex relative h-[10rem] w-[10rem] bg-gradient-to-tr from-[#25252560] from-35% to-[#5e5e5e45]`}
                 onClick={() => handleClick(index)}
+                key={index}
+                variants={variantsBox}
+                animate={selectedCardIndex != index ? "unselected" : "selected"}
               >
-                <Card
-                  key={index}
-                  data={card}
-                  isClicked={index == selectedCard}
-                />
+                <Card data={card} isClicked={index == selectedCardIndex} />
               </motion.div>
             ))}
           </div>
           <button
             onClick={() => {
               let cardNumber =
-                selectedCard == cards.length - 1
+                selectedCardIndex == cards.length - 1
                   ? 0
-                  : selectedCard.valueOf() + 1;
-              setSelectedCard(cardNumber);
+                  : selectedCardIndex + 1;
+              setselectedCardIndex(cardNumber);
             }}
           >
-            Next
+           <span > {">"} </span>
           </button>
         </div>
       </AnimatePresence>
