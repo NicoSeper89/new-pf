ac8cbac7db0edcb95d80a8275c9f4a316b1c95b1
nicoseper89 <nicosepertino@hotmail.com>
2024-02-28 23:37:58 -0300

    change AboutMe struct

--- client/src/app/components/aboutme/AboutMe.tsx
+++ client/src/app/components/aboutme/AboutMe.tsx
@@ -1,14 +1,41 @@
 import React from "react";
+import Image from "next/image";
 
 interface Props {}
 
 const AboutMe: React.FC<Props> = () => {
   return (
     <>
-      <div className="relative flex justify-center items-start w-[85%] h-[70%] pt-16 px-20 bg-[#1B1B1B] rounded-br-[2rem] rounded-[0.5rem] overflow-hidden ">
-        {/* <div className="relative w-full h-[65%] flex justify-center items-center bg-[#535561] rounded-br-[2rem] rounded-[0.5rem]">
-          <div className="relative w-[75%] h-[85%] bg-[#91B933] rounded-br-[2rem] rounded-[0.5rem]"></div>
-        </div> */}
+      <div className="relative h-full w-full flex justify-center items-center text-justify">
+        <div className="relative grid grid-cols-6 grid-rows-6 gap-7 bg-[#1B1B1B] w-[40%] h-[80%] px-8 p-12">
+          <div className="row-span-6 col-span-4 flex flex-col justify-start items-start gap-6">
+            <span className="text-5xl font-local">Aboute Me</span>
+            <p className="text-md">
+              Soy Rosario (Argentina), Full Stack Developer con background en
+              Ingeniería en Sistemas de Información. Terminé hace más de un año
+              el bootcamp en Henry de 800 horas teórico-práctico y desde
+              entonces me capacito a diario sobre el mundo de la programación,
+              tomando cursos, investigando por cuenta propia y realizando
+              proyectos con múltiples tecnologías. Además, perfecciono mi nivel
+              de inglés en una academia de idiomas de la ciudad. Cuento con
+              experiencia en equipos de desarrollo de software utilizando Scrum
+              como metodología ágil, Jira o Trello para organizar tareas y
+              Git/GitHub para el control de versiones.
+            </p>
+          </div>
+          <div className="row-span-2 col-span-2 flex justify-center items-center rounded-full overflow-hidden scale-100">
+            <Image
+              src={"/image-modified.png"}
+              alt="imgprofile"
+              fill
+            />
+          </div>
+          <div className="row-span-4 col-span-2 pt-5 text-md">
+            <p>Adaptabilidad</p>
+            <p>Adaptabilidad</p>
+            <p>Adaptabilidad</p>
+          </div>
+        </div>
       </div>
     </>
   );
